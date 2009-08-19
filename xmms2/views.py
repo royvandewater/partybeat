249cf56b253dedb1c6efe6739a1c96feb56d0835
RoyVandeWater <roy.v.water@gmail.com>
2009-08-18 22:26:27 -0700

    Got it to show track info

--- xmms2/views.py
+++ xmms2/views.py
@@ -1,64 +1,17 @@
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader, RequestContext
-import xmmsclient
-import os
 
 from models import *
+from xmms_controller import Xmms_controller, Player
 
 def player(request):
+  xmms2 = Xmms_controller()
+  player = xmms2.get_player_info()
   return render_to_response('player.html', locals(), context_instance=RequestContext(request))
 
 def run_action(request, action):
-  xmms2 = xmms_controller()
+  xmms2 = Xmms_controller()
   message = xmms2.do_action(action)
+  player = xmms2.get_player_info()
   return render_to_response('player.html', locals(), context_instance=RequestContext(request))
-
-class xmms_controller:
-  def __init__(self):
-    self.xmms = get_xmmsclient()
-
-  def do_action(self, action):
-    if action == "play":
-      return self.play()
-    elif action == "stop":
-      return self.stop()
-    elif action == "pause":
-      return self.pause()
-
-
-  def pause(self):
-    result = self.xmms.playback_pause()
-    result.wait()
-
-    return self.print_playback_error(result, "pause")
-
-
-  def play(self):
-    result = self.xmms.playback_start()
-    result.wait()
-    
-    return self.print_playback_error(result, "play")
-
-
-  def stop(self):
-    result = self.xmms.playback_stop()
-    result.wait()
-    
-    return self.print_playback_error(result, "stop")
-
-
-
-  def print_playback_error(self, result, action):
-    if result.iserror():
-      return("playback %s returned error, %s" % (action, result.get_error()))
-    else:
-      return("playback %s run" % (action)) 
-
-def get_xmmsclient():
-  xmms = xmmsclient.XMMS("xmms2")
-  try:
-    xmms.connect(os.getenv("XMMS_PATH"))
-    return xmms
-  except IOError, detail:
-    return ("Connection failed: %s" % detail)
---
+++ xmms2/xmms_controller.py
@@ -0,0 +1,109 @@
+import xmmsclient
+import os
+
+class Xmms_controller:
+    def __init__(self):
+        self.xmms = self.get_xmmsclient()
+
+    def do_action(self, action):
+        if action == "play":
+            return self.play()
+        elif action == "stop":
+            return self.stop()
+        elif action == "pause":
+            return self.pause()
+
+    def pause(self):
+        result = self.xmms.playback_pause()
+        result.wait()
+        return self.print_playback_error(result, "pause")
+
+
+    def play(self):
+        result = self.xmms.playback_start()
+        result.wait()
+        return self.print_playback_error(result, "play")
+
+
+    def stop(self):
+        result = self.xmms.playback_stop()
+        result.wait()
+        return self.print_playback_error(result, "stop")
+
+
+    def print_playback_error(self, result, action):
+        if result.iserror():
+            return("playback %s returned error, %s" % (action, result.get_error()))
+        else:
+            return("playback %s run" % (action)) 
+
+
+    def get_xmmsclient(self):
+        xmms = xmmsclient.XMMS("xmms2")
+
+        try:
+            xmms.connect(os.getenv("XMMS_PATH"))
+            return xmms
+        except IOError, detail:
+            return ("Connection failed: %s" % detail)
+
+    def get_player_info(self):
+        player = Player()
+        result = self.xmms.playback_current_id()
+        result.wait()
+
+        
+        if result.iserror():
+            player.set_error("Playback current id returns error, %s" % result.get_error())
+            return player
+
+        id = result.value()
+        if id == 0:
+            player.set_error("Nothing is playing")
+            return player
+
+        result = self.xmms.medialib_get_info(id)
+        result.wait()
+
+        if result.iserror():
+            player.set_error("medialib get info returns error, %s" % result.get_error())
+            return player
+
+        minfo = result.value()
+        player.set_info(minfo)
+        return player
+
+
+
+class Player:
+    def __init__(self):
+        self.song   = ""
+        self.artist = ""
+        self.album  = ""
+        self.error  = ""
+    
+    def set_info(self, minfo):
+        self.set_song(minfo)
+        self.set_artist(minfo)
+        self.set_album(minfo)
+
+    def set_error(self, error):
+        self.error = error
+
+    def set_song(self, minfo):
+        try:
+            self.song = minfo["title"]
+        except KeyError:
+            self.song = "unknown"
+
+    def set_artist(self, minfo):
+        try:
+            self.artist = minfo["artist"]
+        except KeyError:
+            self.artist = "unknown"
+
+    def set_album(self, minfo):
+        try:
+            self.album = minfo["album"]
+        except KeyError:
+            self.album = "unknown"
