5cb2639ca787ef0d5bbe53495cf28ce007037196
Roy van de Water <roy.v.water@gmail.com>
2009-11-04 19:41:32 -0700

    Working on using the DB for a cache

--- xmms2/models.py
+++ xmms2/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    error = models.CharField(max_length=255)
+    position = models.IntegerField()
+
+    def __unicode__(self):
+        return ("{0}: {1}".format(self.position, self.title))
+
+class LastUpdate(models.Model):
+    time = models.TimeField()
