57e663375bd94fbb942fdb358044414f25158194
Roy van de Water <roy.v.water@gmail.com>
2009-12-19 10:48:24 -0700

    Updated media root to reflect more likely setting

--- song_storage/admin.py
+++ song_storage/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from xmms2_django.song_storage.models import *
 
 class SongFileAdmin(admin.ModelAdmin):
-    pass
-    # list_display = ('artist', 'album', 'name',) # The future listview
+    list_display = ('artist', 'album', 'name',) # The future listview
 
 admin.site.register(SongFile, SongFileAdmin)
--- song_storage/models.py
+++ song_storage/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+import eyeD3
 
 # Create your models here.
 class SongFile(models.Model):
     file = models.FileField(upload_to="music/%Y/%m/%d")
-    name = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
-    album = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
+    artist = models.CharField(max_length=255, blank=True)
+    album = models.CharField(max_length=255, blank=True)
 
     def __unicode__(self):
         return ("{song.artist} - {song.album} - {song.name}".format(song=self))
 
     def save(self, force_insert=False, force_update=False):
-        # Do stuff
-        # print(dir(self.file))
         # Call the real save method
+        super(SongFile, self).save(force_insert, force_update)
+        # Do stuff
+        tag = eyeD3.Tag()
+        tag.link(self.file.path)
+        self.name = tag.getTitle()
+        self.artist = tag.getArtist()
+        self.album = tag.getAlbum()
+        # Save again!
         super(SongFile, self).save(force_insert, force_update)
 
