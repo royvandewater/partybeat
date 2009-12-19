b07a4dac2fbc706abe80e629016b013543f0958e
Roy van de Water <roy.v.water@gmail.com>
2009-12-19 10:05:26 -0700

    Basic file upload and storage

---
+++ song_storage/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from xmms2_django.song_storage.models import *
+
+class SongFileAdmin(admin.ModelAdmin):
+    pass
+    # list_display = ('artist', 'album', 'name',) # The future listview
+
+admin.site.register(SongFile, SongFileAdmin)
--- song_storage/models.py
+++ song_storage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class SongFile(models.Model):
+    file = models.FileField(upload_to="music/%Y/%m/%d")
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return ("{song.artist} - {song.album} - {song.name}".format(song=self))
+
+    def save(self, force_insert=False, force_update=False):
+        # Do stuff
+        # print(dir(self.file))
+        # Call the real save method
+        super(SongFile, self).save(force_insert, force_update)
+
