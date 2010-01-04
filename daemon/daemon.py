bcadd647393da78288d5ca1ff449f566cd883e9c
Roy van de Water <roy.v.water@gmail.com>
2010-01-03 22:38:22 -0700

    daemon available from ./manage.py, basic initialization and loop created

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -1,7 +1,18 @@
+from django.core.management.base import BaseCommand
 from xmms_controller import Xmms_controller
 
-def main(argv):
-    xmms_controller = Xmms_controller()
+import sys
+import time
 
-if __name__ == "__main__":
-    main(argv[1:])
+def main(argv):
+    # perform initialization
+    xmms_controller = Xmms_controller()
+    
+    # enter main loop
+    try:
+        while True:
+            xmms_controller.get_player_info()
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\nDaemon terminating")
+        sys.exit(0)
