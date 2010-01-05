97de1739c4e0dfaedaa86273cac1f2778a129420
Roy van de Water <roy.v.water@gmail.com>
2010-01-05 09:39:47 -0700

    Removed daemon models and admin controlls from player app

--- player/models.py
+++ player/models.py
@@ -1,18 +1,1 @@
 from django.db import models
-
-# Create your models here.
-class Song(models.Model):
-    name = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
-    album = models.CharField(max_length=255)
-    error = models.CharField(max_length=255)
-    position = models.IntegerField()
-    xmms_id = models.IntegerField()
-
-    def __unicode__(self):
-        return ("{0}: {1}".format(self.position, self.name))
-
-class XmmsStatus(models.Model):
-    timeout = models.IntegerField(help_text="Cache refresh time in milliseconds, set too low and xmms2 will crash")
-    current_action = models.IntegerField(help_text="0: Stopped, 1: Playing, 2: Paused")
-    last_update = models.DateTimeField()
