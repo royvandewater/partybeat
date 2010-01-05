8ff31368ded0acfe7493be148cd8d6a212114a6a
Roy van de Water <roy.v.water@gmail.com>
2010-01-05 12:49:51 -0700

    Allowed user override of songFile id3 scanning

--- song_storage/admin.py
+++ song_storage/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from xmms2_django.song_storage.models import *
 
 class SongFileAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'album', 'name',) # The future listview
+    list_display = ('name', 'album', 'artist',) # The future listview
+    fieldsets = (
+            (None, {
+                'fields': ('file',)
+            }),
+            ('Leave blank to extract from id3', {
+                'fields': ('name', 'artist', 'album')
+            }),
+    )
 
 admin.site.register(SongFile, SongFileAdmin)
--- song_storage/models.py
+++ song_storage/models.py
@@ -21,35 +21,37 @@ class SongFile(models.Model):
     def save(self, force_insert=False, force_update=False):
         # Call the real save method
         super(SongFile, self).save(force_insert, force_update)
-        # Get the filetype
-        filetype = (self.file.name.rpartition(".")[2]).lower()
-        if filetype == "mp3":
-            from mutagen.mp3 import MP3
-            mp3 = MP3(self.file.path)
+        # If name, artist and album are set, user is overriding id3 read
+        if not (self.name and self.artist and self.album):
+            # Get the filetype
+            filetype = (self.file.name.rpartition(".")[2]).lower()
+            if filetype == "mp3":
+                from mutagen.mp3 import MP3
+                mp3 = MP3(self.file.path)
 
-            self.name = mp3["TIT2"]
-            self.artist = mp3["TPE1"]
-            self.album = mp3["TALB"]
-        elif filetype == "flac":
-            from mutagen.flac import FLAC
-            flac = FLAC(self.file.path)
+                self.name = mp3["TIT2"]
+                self.artist = mp3["TPE1"]
+                self.album = mp3["TALB"]
+            elif filetype == "flac":
+                from mutagen.flac import FLAC
+                flac = FLAC(self.file.path)
 
-            self.name = self.print_list(flac["title"])
-            self.artist = self.print_list(flac["artist"])
-            self.album = self.print_list(flac["album"])
+                self.name = self.print_list(flac["title"])
+                self.artist = self.print_list(flac["artist"])
+                self.album = self.print_list(flac["album"])
 
-        elif filetype == "ogg":
-            from mutagen.oggvorbis import OggVorbis
-            ogg = OggVorbis(self.file.path)
+            elif filetype == "ogg":
+                from mutagen.oggvorbis import OggVorbis
+                ogg = OggVorbis(self.file.path)
 
-            self.name = self.print_list(ogg["title"])
-            self.artist = self.print_list(ogg["artist"])
-            self.album = self.print_list(ogg["album"])
+                self.name = self.print_list(ogg["title"])
+                self.artist = self.print_list(ogg["artist"])
+                self.album = self.print_list(ogg["album"])
 
-        else:
-            self.name = self.file.name.rpartition("/")[2]
-            self.artist = "Unknown"
-            self.album = "Unknown"
-        # Save again!
-        super(SongFile, self).save(force_insert, force_update)
+            else:
+                self.name = self.file.name.rpartition("/")[2]
+                self.artist = "Unknown"
+                self.album = "Unknown"
+            # Save again!
+            super(SongFile, self).save(force_insert, force_update)
 
