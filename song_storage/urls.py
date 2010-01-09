83812c2079a4974761e97aa82bca341312de10a1
Roy van de Water <roy.v.water@gmail.com>
2010-01-09 13:32:30 -0700

    Removed duplicate url parameter in song_storage

--- song_storage/urls.py
+++ song_storage/urls.py
@@ -6,13 +6,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('xmms2_django.song_storage.views',
     # Library files
-    (r'^library/$', 'library'),
-    (r'^library/add/(?P<song_id>\w+)/$', 'add'),
-    (r'^library/artists/$', 'artists'),
-    (r'^library/albums/$', 'albums'),
-    (r'^library/albums/(?P<artist>\w+)/$', 'albums'),
-    (r'^library/songs/$', 'songs'),
-    (r'^library/by_album/(?P<album>\w+)/$', 'songs'),
-    (r'^library/songs/(?P<artist>\w+)/$', 'songs'),
-    (r'^library/songs/(?P<artist>\w+)/(?P<album>\w+)/$', 'songs'),
+    (r'$', 'library'),
+    (r'add/(?P<song_id>\w+)/$', 'add'),
+    (r'artists/$', 'artists'),
+    (r'albums/$', 'albums'),
+    (r'albums/(?P<artist>\w+)/$', 'albums'),
+    (r'songs/$', 'songs'),
+    (r'by_album/(?P<album>\w+)/$', 'songs'),
+    (r'songs/(?P<artist>\w+)/$', 'songs'),
+    (r'songs/(?P<artist>\w+)/(?P<album>\w+)/$', 'songs'),
 )
