3526724141e9fa292e413b62e83baefcffd01160
Roy van de Water <roy.v.water@gmail.com>
2010-01-10 05:12:14 -0700

    Made editform elements optional so it defaults back to id3 on blank

--- song_storage/forms.py
+++ song_storage/forms.py
@@ -4,6 +4,6 @@ class UploadForm(forms.Form):
     file = forms.FileField()
 
 class EditForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    artist = forms.CharField(max_length=255)
-    album = forms.CharField(max_length=255)
+    name = forms.CharField(max_length=255, required=False)
+    artist = forms.CharField(max_length=255, required=False)
+    album = forms.CharField(max_length=255, required=False)
