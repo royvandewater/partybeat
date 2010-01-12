4c4d9bf01acbece97b893f6027d5a0cf00c83feb
Roy van de Water <roy.v.water@gmail.com>
2010-01-11 21:07:47 -0700

    Set up nescessary fixtures for player testing

--- player/tests.py
+++ player/tests.py
@@ -8,16 +8,11 @@ Replace these with more appropriate tests for your application.
 from django.test import TestCase
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    fixtures = ['daemon.yaml']
+
+    def test_info(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that the info call returns 200
         """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+        response = self.client.get('/player/info/')
+        self.failUnlessEqual(response.status_code, 200)
