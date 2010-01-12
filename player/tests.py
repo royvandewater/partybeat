5a7db1e1c6783827db7c419e75815331b9602ad1
Roy van de Water <roy.v.water@gmail.com>
2010-01-11 21:24:34 -0700

    Added tests to player

--- player/tests.py
+++ player/tests.py
@@ -7,12 +7,28 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
+
 class SimpleTest(TestCase):
     fixtures = ['daemon.yaml']
 
+
+    def check_response_code(self, url, code):
+        """
+        Check the url to ensure it returns the proper response code
+        """
+        response = self.client.get(url)
+        self.failUnlessEqual(response.status_code, code)
+
+    # These tests check all urls in the urls.py file
     def test_info(self):
-        """
-        Tests that the info call returns 200
-        """
-        response = self.client.get('/player/info/')
-        self.failUnlessEqual(response.status_code, 200)
+        self.check_response_code('/player/info/', 200)
+
+    def test_playlist(self):
+        self.check_response_code('/player/playlist/', 200)
+
+    def test_action(self):
+        self.check_response_code('/player/action/play/', 302)
+
+    def test_delete(self):
+        self.check_response_code('/player/delete/1/', 302)
+
