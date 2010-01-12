0f7c5a41378ad7297fed8c0749d059f5d45175a6
Roy van de Water <roy.v.water@gmail.com>
2010-01-12 02:48:52 -0700

    Added lightbox to editform

--- media/js/library.js
+++ media/js/library.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
 
-        $(".library_item_add > a").click(function(e) {
-            // Disable the default click behaviour
-            e.preventDefault();
+    $(".library_item_add > a").click(function(e) {
+        // Disable the default click behaviour
+        e.preventDefault();
 
-            // Get this object's url
-            var target_url = $(this).attr("href");
-            
-            // Send Request (and don't wait for a response)
-            $.post(target_url, {source: "ajax"});
+        // Get this object's url
+        var target_url = $(this).attr("href");
+        
+        // Send Request (and don't wait for a response)
+        $.post(target_url, {source: "ajax"});
 
 
-            // Update info is from player.js, forces the playlist to update
-            setTimeout("update_info()", 200); 
-        });
+        // Update info is from player.js, forces the playlist to update
+        setTimeout("update_info()", 200); 
+    });
+
+    $(".library_item_edit > a").fancybox({
+        'frameWidth': 450,
+        'frameHeight': 180,
+        'overlayOpacity': 0.6,
+        'hideOnContentClick': false,
+    });
 });
