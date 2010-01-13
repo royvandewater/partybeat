ef37917a7edd03c7dd74726255067d72503b494d
Roy van de Water <roy.v.water@gmail.com>
2010-01-12 21:00:08 -0700

    progress interpolation complete

--- media/js/player.js
+++ media/js/player.js
@@ -1,11 +1,24 @@
+// Percentage of the progress bar that is increased each 100 ms
+var progress_modifier = 0.0;
+// Percentage of the song we have progressed to
+var current_progress = 0.0;
+var is_playing = false;
+
 $(document).ready(function() {
 
-    $(document).everyTime(2000, function() { 
-        update_info();
+    update_info();
+    update_playlist();
+
+    $(document).everyTime(500, update_info);
+    $(document).everyTime(2000, update_playlist);
+
+    $(document).everyTime(100, function() {
+        if( is_playing )
+            interpolate_progress_bar();
     });
 
     // Send the player a command without caring about the return value
-    $(".action > p a").click(function(e) {
+    $(".action > a").click(function(e) {
         // Disable the default behaviors of all our action links         
         e.preventDefault();
 
@@ -21,6 +34,7 @@ $(document).ready(function() {
         // We'll want to force a player status update after 200 milliseconds
         // because the daemon needs time to write to the db
         setTimeout("update_info()", 200); 
+
     });
 
     $("#playlist > div span a").live("click", function(e) {
@@ -53,12 +67,15 @@ function update_info() {
             $("player_status current_action", xml).text());
 
         // Get progress on current track
-        var seek = $("player_status seek");
-        var max_seek = $("player_status max_seek");
+        var seek = $("player_status seek", xml).text();
+        var max_seek = $("player_status max_seek", xml).text();
+        progress_modifier = (100 / max_seek);
+        current_progress = seek / max_seek;
+        var offset = current_progress * 500;
 
         // Decide whether to display the play button or the pause button
         var is_playing_string = $("player_status is_playing", xml).text();
-        var is_playing = string_to_boolean(is_playing_string);
+        is_playing = string_to_boolean(is_playing_string);
 
         if(is_playing) {
             $("#xmms_play").addClass("hidden");
@@ -67,12 +84,20 @@ function update_info() {
             $("#xmms_play").removeClass("hidden");
             $("#xmms_pause").addClass("hidden");
         }
+    });
+}
 
+function update_playlist() {
         // Load in data from playlist
         target_url = "/player/playlist/";
         $("#playlist").load(target_url);
-        
-    });
+}
+
+function interpolate_progress_bar() {
+    current_progress += progress_modifier;
+    var offset = current_progress * 471;
+    if(offset < 485)
+        $("#seek_head").css('left', offset);
 }
 
 function string_to_boolean(boolean_string) {
