931875e71e519fd3bbd31fa70e262f3aa23abfc2
Roy van de Water <roy.v.water@gmail.com>
2010-01-14 22:21:59 -0700

    Hover and highlight work

--- media/js/library.js
+++ media/js/library.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+    $(".library_item").hover(
+        function(e) {
+            $(this).addClass("ui-state-hover");
+        },
+        function(e) {
+            $(this).removeClass("ui-state-hover");
+        });
+
     $(".library_item_add > a").click(function(e) {
         // Disable the default click behaviour
         e.preventDefault();
@@ -26,5 +34,5 @@ $(document).ready(function() {
     // Uncomment for ajax upload
     $("#library_upload a").fancybox(fancybox_settings);
 
-    $('.library_item').corner('4px');
+    // $('.library_item').corner('4px');
 });
