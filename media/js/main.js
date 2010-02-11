1e28c993360f0bb8b4450de73831ad685562a88e
Roy van de Water <roy.v.water@gmail.com>
2010-02-10 20:24:43 -0700

    It seems as if themeroller was moved

--- media/js/main.js
+++ media/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-    $("#theme_roller").themeswitcher();
-    $("#theme_roller_link").click( function(e) {
-        e.preventDefault();
-        $("#theme_roller").slideToggle();
-    });
+    // $("#theme_roller").themeswitcher();
+    // $("#theme_roller_link").click( function(e) {
+        // e.preventDefault();
+        // $("#theme_roller").slideToggle();
+    // });
 });
